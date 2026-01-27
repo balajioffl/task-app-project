d114e1a52b737da383219c9f07c51121e24df9cb
balajioffl <balajioffl07@gmail.com>
2026-01-27 15:45:28 +0530

    workflow fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,14 +104,6 @@ DATABASES = {
     }
 }
 
-if os.getenv("GITHUB_ACTIONS") == "true":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -244,19 +236,3 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
-if os.getenv("GITHUB_ACTIONS") == "true":
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "channels.layers.InMemoryChannelLayer",
-        }
-    }
-else:
-    CHANNEL_LAYERS = {
-        "default": {
-            "BACKEND": "channels_redis.core.RedisChannelLayer",
-            "CONFIG": {
-                "hosts": [("127.0.0.1", 6379)],
-            },
-        }
-    }
